9f37eda74c2ec98b959171128c0a738e67aa5901
Roy Lopez <roylopezdev@gmail.com>
2023-06-14 12:26:51 -0300

    add home route

--- server.js
+++ server.js
@@ -6,9 +6,19 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = process.env.PORT || 5002;
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
 
 // Connect to MongoDB and start server
+const PORT = process.env.PORT || 5002;
 
 mongoose
   .connect(process.env.MONGODB_URI)
