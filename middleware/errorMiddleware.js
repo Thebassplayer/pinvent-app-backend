0d32f0d6bb5a7ae297c6e23d1f45b1efae5c39cb
Roy Lopez <roylopezdev@gmail.com>
2023-06-27 19:20:11 -0300

    upgrade errorMiddleware.js

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,8 +1,14 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
-  console.log("Error Handler", err);
-  res.status(statusCode);
-  res.json({
+
+  // Handle MongoDB connection error
+  if (err.name === "MongoError") {
+    res.status(500).json({ error: "Failed to connect to the database" });
+    return;
+  }
+
+  // Handle other errors
+  res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === "development" ? err.stack : null,
   });
